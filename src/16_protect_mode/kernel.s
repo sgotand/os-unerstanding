0c32ff2c9a0a127acb867b36db905c3eb790a9ef
Shotaro Gotanda <g.sho1500@gmail.com>
2024-03-12 00:35:44 +0900

    prepare codes for section 14.17
    Signed-off-by: Shotaro Gotanda <g.sho1500@gmail.com>

---
+++ src/16_protect_mode/kernel.s
@@ -0,0 +1,15 @@
+; vim: set ft=nasm nospell:
+
+%include "../include/define.s"
+%include "../include/macro.s"
+
+ORG KERNEL_LOAD
+
+[BITS 32]
+; entry point
+
+kernel:
+  jmp $
+
+; padding
+  times KERNEL_SIZE - ($ - $$) db 0;
